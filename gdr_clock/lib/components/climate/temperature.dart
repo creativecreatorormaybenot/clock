ddf4ee9843b6f88b66ef61102c82ef37a3b210b5
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2019-12-25 19:53:44 +0000

    Set up constraints for design

--- gdr_clock/lib/components/climate/temperature.dart
+++ gdr_clock/lib/components/climate/temperature.dart
@@ -45,6 +45,11 @@ class Temperature extends LeafRenderObjectWidget {
 }
 
 class RenderTemperature extends RenderCompositionChild {
+  static const temperatureScale = {
+    TemperatureUnit.celsius: [-16, 50],
+    TemperatureUnit.fahrenheit: [3, 122],
+  };
+
   RenderTemperature({
     this.unit,
     this.unitString,
@@ -62,7 +67,7 @@ class RenderTemperature extends RenderCompositionChild {
 
   @override
   void performResize() {
-    size = Size(constraints.biggest.width, constraints.biggest.height / 1.3);
+    size = Size(constraints.biggest.width, constraints.biggest.height / 1.2);
   }
 
   @override
@@ -72,25 +77,65 @@ class RenderTemperature extends RenderCompositionChild {
     canvas.save();
     canvas.translate(offset.dx, offset.dy);
 
+    final area = RRect.fromRectAndRadius(Offset.zero & size, Radius.circular(size.width / 36));
+
+    // Background
+    final gradient = LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: const [
+      Color(0xffcc9933),
+      Color(0xffc9bd6c),
+    ]);
     canvas.drawRRect(
-        RRect.fromRectAndRadius(Offset.zero & size, Radius.circular(size.width)),
+        area,
+        Paint()
+          ..style = PaintingStyle.fill
+          ..shader = gradient.createShader(Offset.zero & size));
+
+    // Border
+    canvas.drawRRect(
+        area,
         Paint()
           ..style = PaintingStyle.stroke
           ..color = const Color(0xff000000));
 
-    final unitIndent = size.width / 6,
+    // Some kind of brad nails at the top and bottom
+    final bradRadius = size.width / 29,
+        // Lighter in the center to give some depth based on the lighting
+        bradGradient = const RadialGradient(colors: [
+      Color(0xff898984),
+      Color(0xff43464b),
+    ]),
+        bradIndent = size.width / 11;
+    () {
+      final topRect = Rect.fromCircle(center: Offset(size.width / 2, bradIndent), radius: bradRadius);
+      canvas.drawOval(topRect, Paint()..shader = bradGradient.createShader(topRect));
+
+      final bottomRect = Rect.fromCircle(center: Offset(size.width / 2, size.height - bradIndent), radius: bradRadius);
+      canvas.drawOval(bottomRect, Paint()..shader = bradGradient.createShader(bottomRect));
+    }();
+
+    final unitIndent = size.width / 8,
         unitPainter = TextPainter(
       text: TextSpan(
         text: unitString,
         style: TextStyle(
           color: const Color(0xff000000),
-          fontSize: size.width / 5,
+          fontSize: size.width / 6,
         ),
       ),
       textDirection: TextDirection.ltr,
-    ), freeUnitWidth = size.width - unitIndent * 2;
+    ),
+        freeUnitWidth = size.width - unitIndent * 2;
     unitPainter.layout(maxWidth: freeUnitWidth);
-    unitPainter.paint(canvas, Offset(unitIndent + (freeUnitWidth / 2 - unitPainter.width / 2), unitIndent));
+    unitPainter.paint(canvas, Offset(unitIndent + (freeUnitWidth / 2 - unitPainter.width / 2), unitIndent + bradIndent));
+
+    // Constraints for the positioning of the numbers, lines, brackets, and tube.
+    final addedIndentFactor = 2.7,
+        mount = Line.fromBH(bottom: size.height - bradIndent * addedIndentFactor, height: size.height / 13),
+        tube = Line(bottom: mount.top, top: unitIndent + bradIndent * addedIndentFactor),
+        brackets = Line.fromTBI(top: tube.top, bottom: tube.bottom, indent: tube.height / 6.42),
+        lines = Line.fromTBI(top: brackets.top, bottom: brackets.bottom, indent: -mount.height / 3);
+
+    // Glass tube
 
     canvas.restore();
   }
--- gdr_clock/lib/ui/geometry.dart
+++ gdr_clock/lib/ui/geometry.dart
@@ -4,3 +4,18 @@ import 'dart:ui';
 ///
 /// Needs a name in order to be accessible from within other files.
 extension ExtendedRect on Rect {}
+
+/// Line with functionality tailored to the needs of this clock challenge entry.
+class Line {
+  final double top, bottom;
+
+  const Line({this.top, this.bottom});
+
+  factory Line.fromTH({double top, double height}) => Line(top: top, bottom: top + height);
+
+  factory Line.fromBH({double bottom, double height}) => Line(top: bottom - height, bottom: bottom);
+
+  factory Line.fromTBI({double top, double bottom, double indent}) => Line(top: top + indent, bottom: bottom - indent);
+
+  double get height => bottom - top;
+}
